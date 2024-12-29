3bad1fc27d2f1ca7f0b2a677eeca0c68af2b8199
ABC@xyz <137751558+ABC11xyz@users.noreply.github.com>
2024-12-29 11:28:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5100;
 const corsOptions = {
   origin: ['https://train-ticket-rvn.vercel.app'], 
   methods: ['GET', 'POST'], 
+  credentials: true,
 };
 
 app.use(cors(corsOptions)); 
