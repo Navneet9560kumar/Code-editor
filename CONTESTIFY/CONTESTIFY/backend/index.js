c2be628e274c62b839b16baeee1a40482a6a734b
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-02-06 22:56:52 +0530

    Set up a backend and authentication rest of debuge

--- CONTESTIFY/CONTESTIFY/backend/index.js
+++ CONTESTIFY/CONTESTIFY/backend/index.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
-const dotenv = require('./config/dotenv');
-const authRoutes = require('./routes/authRoutes');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
-
-// Middleware
-app.use(cookieParser());
-app.use(express.json()); // For parsing application/json
-
-// Connect to MongoDB
-connectDB();
-
-// Routes
-app.use('/api/auth', authRoutes);
-
-// Start server
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
+app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
