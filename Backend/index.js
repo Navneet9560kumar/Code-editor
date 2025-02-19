9f2b22a4e7ad10eb2cee6a4a1a789d4a369910e5
dhanashri-25 <137794519+dhanashri-25@users.noreply.github.com>
2025-02-19 23:13:37 +0530

    final

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import connectDB from './DB/dbConnect.js';
-import TrainRoutes from './Routes/TrainRoutes.js'
-
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./DB/dbConnect.js";
+import TrainRoutes from "./Routes/TrainRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
@@ -10,19 +10,21 @@ const app = express();
 
 const PORT = process.env.PORT || 5100;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://train-ticket-rvn.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST"],
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello......");
+});
 
 app.use("/api/trains", TrainRoutes);
 
-
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log(`Server is running at : http://localhost:${PORT}`);
-})
+});
